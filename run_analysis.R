b59693e30ecba1ad9d354eec2ba6fd398e05afaf
Vasilis Michailidis <100042190+basilismich@users.noreply.github.com>
2022-05-12 12:02:32 +0300

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,9 +1,9 @@
-#Merges the training and the test sets to create one data set.
+##Merges the training and the test sets to create one data set.
 
- Extracts only the measurements on the mean and standard deviation for each measurement. 
+ #Extracts only the measurements on the mean and standard deviation for each measurement. 
 
- Uses descriptive activity names to name the activities in the data set
+ #Uses descriptive activity names to name the activities in the data set
 
- Appropriately labels the data set with descriptive variable names. 
+ #Appropriately labels the data set with descriptive variable names. 
 
- From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+ #From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
